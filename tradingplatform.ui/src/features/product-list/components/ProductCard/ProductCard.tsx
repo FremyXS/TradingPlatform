647d19eda239aa65f6b06ee227314bd15f23338d
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-03-28 23:43:30 +0500

    fix: change problems

--- tradingplatform.ui/src/features/product-list/components/ProductCard/ProductCard.tsx
+++ tradingplatform.ui/src/features/product-list/components/ProductCard/ProductCard.tsx
@@ -3,15 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { ProducType } from "../../../../types";
 import './ProductCard.scss'
 
-function ProductCard({product} : {product: ProducType}){
+function ProductCard({ product }: { product: ProducType }) {
     const navigate = useNavigate();
     return (
         <Link className="product-card"
-        to={`product/${product.id}`}>
-            <img 
-            src="https://7themes.su/_ph/28/313544153.jpg?1674990037" 
-            title="game"/>
-            <div className="product-card__info">                
+            to={`product/${product.id}`}>
+            <img
+                src={product.image_url}
+                title="game" />
+            <div className="product-card__info">
                 <div>{product.title}</div>
                 <div>{product.price}</div>
             </div>
