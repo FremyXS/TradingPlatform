270827b22991348fd03f3b1f4abba14fd16d34c3
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-04-06 06:46:18 +0500

    feat: add method controller and to router

--- tradingplatform.sales/app/Http/Controllers/OrdersController.php
+++ tradingplatform.sales/app/Http/Controllers/OrdersController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Orders;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Validator;
 
 class OrdersController extends Controller
 {
-    //
+    function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'orders' => [
+                'product_id' => 'required',
+                'buyer_id' => 'required',
+                'seller_id' => 'required',
+                'status' => 'required'
+            ]
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $order = Orders::create($validator->validated());
+
+        return response($order, Response::HTTP_CREATED);
+    }
 }
